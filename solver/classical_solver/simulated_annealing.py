657e476a10e248c63f1bc84d2d6923c959a0228e
Romain <romainbernardmassias50@gmail.com>
2026-03-09 16:20:22 +0100

    Adding simulated_annealing.py

---
+++ solver/classical_solver/simulated_annealing.py
@@ -0,0 +1,8 @@
+# TODO 2: Code a simulated annealing method to solve the QUBO problem. Assume you have the
+# QUBO matrix as input (which can be evaluated as xTQx). The use of the method should provide at
+# least the following parameters:
+# • Cooling schedule law and decreasing_factor (we will assume at first an exponential decrease
+# of the temperature, but in practice you could use any law, for example linear or geometric).
+# The decreasing_factor relates to the constant used in the slides of the lesson.
+# • Number of fixed steps per temperature step.
+# • Stopping criteria (temperature threshold).
