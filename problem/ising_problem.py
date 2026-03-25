0b1fb30fb4cde86809b33984d2f099128355696f
Romain <romainbernardmassias50@gmail.com>
2026-03-25 21:50:00 +0100

    Cleaning ising_problem

--- problem/ising_problem.py
+++ problem/ising_problem.py
@@ -43,32 +43,3 @@ class IsingProblem:
                 subs_dict[var] = val
         cost = self.expr.subs(subs_dict)
         return float(cost)
-
-
-# # TODO 14: Create a toy example and check that your code is working as expected.
-
-# G_test = nx.Graph()
-# G_test.add_node('s1', weight=-1.0)
-# G_test.add_node('s2') 
-# G_test.add_node('s3')
-# G_test.add_edge('s1', 's2', weight=2.0)
-# G_test.add_edge('s2', 's3', weight=-1.0)
-# G_test.add_edge('s1', 's3', weight=1.5)
-
-# string_test = "+2*s1*s2 -1*s1 +3*s2*s3 +4*s2 -5*s3*s1+ 8*s3"
-
-# mon_probleme_1 = IsingProblem(string= string_test)
-# mon_probleme_2 = IsingProblem(graph= G_test)
-
-# ma_solution = {
-#     's1': 1,
-#     's2': -1,
-#     's3': 1
-# }
-
-# print(f"1.   La formule est {mon_probleme_1.to_sympy_expr()} et contient {mon_probleme_1.variables}")
-# print(f"Pour la configuration {ma_solution},le coût total calculé est : {mon_probleme_1.get_cost(ma_solution)} ")
-
-# print(f"2.   La formule est {mon_probleme_2.to_sympy_expr()} et contient {mon_probleme_2.variables}")
-# print(f"Pour la configuration {ma_solution},le coût total calculé est : {mon_probleme_2.get_cost(ma_solution)} ")
-
