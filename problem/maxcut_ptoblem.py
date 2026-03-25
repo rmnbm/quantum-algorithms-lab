945f6f490436c82fee197e5cb6c8b66389074553
ADYEL <ilyes.adyel@edu.devinci.fr>
2026-03-25 15:59:11 +0100

    .

--- problem/maxcut_ptoblem.py
+++ problem/maxcut_ptoblem.py
@@ -1,5 +1,8 @@
+import sympy as sp
+import networkx as nx
+
 # TODO 1: Create a class MaxCutProblem that can be instanciated with a networkx graph.
-class MaxCutProblem: 
+class MaxCutProblem:
     def __init__(self, graph):
         self.graph = graph
         self._sympy_expr = None
@@ -8,22 +11,32 @@ class MaxCutProblem:
 #         Once computed, store it in a class attribute and check if this attribute exists before the computation (it can be costly to do it several times)
 #         You can use the sympify function of the sympy library to create the expression
 #         See the documentation here: https://docs.sympy.org/latest/tutorials/intro-tutorial/basic_operations.html
-    def to_sympy_expr():
+    def to_sympy_expr(self):
         if self._sympy_expr is None:
             expr_str = ""
-            for u, v in self.graph.edges(data = true):
-                weight = data.get("weight", 1)
-                term = f"-{weight} * (x_{u} * x_{v})"
-                expr_str += term + " " 
 
-            self._sympy_expr = sympify(expr_str.replace(" -", " - " ).replace(" +", " + "))
+            for u, v, data in self.graph.edges(data=True):
+                weight = data.get("weight", 1)
+                term = f"-{weight}*(x_{u}*x_{v})"
+                expr_str += term + " + "
+
+            if expr_str.endswith(" + "):
+                expr_str = expr_str[:-3]
+
+            if expr_str == "":
+                expr_str = "0"
+
+            self._sympy_expr = sp.sympify(expr_str)
+
         return self._sympy_expr
 
 
 #
 # TODO 3: Create a method eval(solution) that evaluates the cost associated to a solution. The solution could be a dictionnary with string keys
 # (variables of the cost function) and integer values. You can use the sympy subs method (do not forget to cast your strings to symbols).
-    def eval(solution):
+    def eval(self, solution):
         expr = self.to_sympy_expr()
-        subs_dict = {symbol}
+        subs_dict = {sp.Symbol(var): value for var, value in solution.items()}
+        return expr.subs(subs_dict)
         
+
